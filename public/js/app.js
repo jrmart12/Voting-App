dabca56d0f9c53c7d39ed325ce687064dce36ce1
jrmart12 <rodolfojose@live.com>
2018-03-07 23:32:37 -0600

    app 9 with immutability

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,32 @@
 class ProductList extends React.Component {
-	handleProductUpVote(productId) {
-		console.log(productId + ' was upvoted.');
+	constructor(props) {
+		super(props);
+		this.state = {
+			products: [],
+		};
+		this.handleProductUpVote = this.handleProductUpVote.bind(this);
 	}
-	render() {	
-		const products = Seed.products.sort((a, b) => (
+	componentDidMount() {
+		this.setState({ products: Seed.products });
+	}
+	handleProductUpVote(productId) {
+		const nextProducts = this.state.products.map((product) => {
+			if (product.id === productId) {
+				return Object.assign({}, product, {
+					votes: product.votes + 1,
+				});
+			} else {
+				return product;
+			}
+		});
+		this.setState({
+			products: nextProducts,
+		});
+	}
+	render() {
+		const products = this.state.products.sort((a, b) => (
 		b.votes - a.votes
-		));
+	));
 		const productComponents = products.map((product) => (
 		<Product
 		key={'product-' + product.id}
